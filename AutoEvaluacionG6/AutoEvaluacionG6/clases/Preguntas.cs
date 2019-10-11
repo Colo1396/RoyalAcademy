9ddaf9be601850d82aa24020ce84605350b0beef
Nazareno <nazarenogalvan@outlook.com>
2019-10-11 19:59:18 -0300

    Se empezo a armar el abm de preguntas

---
+++ AutoEvaluacionG6/AutoEvaluacionG6/clases/Preguntas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoEvaluacionG6.clases
+{
+    public class Preguntas
+    {
+        public int idPregunta { get; set; }
+        public int tipoPregunta { get; set; }
+        public string consigna { get; set; }
+
+    }
+    
+
+
+}
