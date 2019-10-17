665e26fedb8636496d3bea9862dd8a678aa52e4c
Nazareno <nazarenogalvan@outlook.com>
2019-10-17 00:55:31 -0300

    Intentado Alta Pregunta

--- AutoEvaluacionG6/AutoEvaluacionG6/web/scripts/altaPreguntas.js
+++ AutoEvaluacionG6/AutoEvaluacionG6/web/scripts/altaPreguntas.js
@@ -1,6 +1,77 @@
 ﻿
-$(document).ready(function () {
+/*$(document).ready(function () {
     $("#btnTipoPregunta").click(function () {
         console.log('se clikeo boton');
     });
 });
+*/
+
+alert("entre al js");
+
+$(document).ready(function myfunction() {
+    $("#btn_Alta_preg").click(function () {
+        alert("se clikeo el boton");
+        validadAltaPregunta()
+    });
+
+});
+
+/*windows.onload = function () {
+
+    var btn_enviarAlta = $("#btn_Alta_preg");
+
+     btn_enviarAlta.on("click", function () {
+        alert("se clikeo el boton");
+        validadAltaPregunta()
+        
+    });
+   
+}*/
+
+/**
+ * Metodo Ajax para las llamadas a WS, estructura basica.
+ * @param {any} paramJSON  : son los parametros que enviara al Ws , en este caso son de tipo JSON,  aunque igual se pasa a cadena con el metodo JSON.stringify().
+ * @param {any} urlWS      : es la Url donde se aloja el WEbService a llamar.
+ * @param {any} asincrono  : indica si la llamada es asincronica, generalmente es false (llamada sincronica, espera retorno)
+ */
+
+
+function llamarWS(paramJSON, urlWS, asincrono) {
+    var retorno = null;
+    $.ajax({
+        type: "POST",
+        url: urlWS,
+        data: JSON.stringify(paramJSON),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: asincrono,
+        success: function (respuesta) {
+            retorno = respuesta.d;
+        }
+    });
+    return retorno;
+
+}
+
+function validadAltaPregunta() {
+    var idPregunta = $("#idPregunta").val();
+    var idTipoPregunta = $("#idTipoPregunta").val();
+    var consigna = $("#consigna").val();
+
+    var urlCompletaAltaPregunta = "/ws/altaPreguntas.asmx/AltaPregunta"
+
+    var parametros = {
+        "idPregunta": idPregunta,
+        "idTipoPregunta": idTipoPregunta,
+        "consigna": consigna
+    }
+
+    var retorno = llamarWS(parametros, urlCompletaAltaPregunta, false);
+
+    if (retorno == "true") {
+        // redireccion al menu
+        location.href = "menu.html";
+    } else {
+        alert("No se pudo enviar alta de pregunta");
+    }
+}
---
+++ AutoEvaluacionG6/AutoEvaluacionG6/ws/altaPreguntas.asmx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data.MySqlClient;
+using AutoEvaluacionG6.conexion;
+
+namespace AutoEvaluacionG6.ws
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string AltaPregunta(int idPregunta,int idTipoPregunta,String consigna)
+        {
+            //String sql = "insert into pregunta (idPregunta,idTipoPregunta,consigna) values ('" + idPregunta + "','" + idTipoPregunta + "','" + consigna + "')";
+            String sql = "INSERT INTO user(`idPregunta`, `idTipoPregunta`, `consigna`) VALUES (NULL, '" + idPregunta + "', '" + idTipoPregunta + "', '" + consigna + "')";
+            MySqlConnection connection = null;
+            MySqlDataReader lector = null;
+
+            String retorno = "false";
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                connection = Conexion.getConexion();
+                cmd.Connection = connection;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = sql;
+                cmd.CommandTimeout = 240;
+                connection.Open();
+
+                lector = cmd.ExecuteReader();
+                /*if (lector.HasRows)
+                {
+                    while (lector.Read())
+                    {
+                    }
+                    retorno = "true";
+                }*/
+
+                if (lector != null) lector.Close();
+                if (connection != null) connection.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error durante el inicio de sesión!" + ex.Message);
+            }
+           /* finally
+            {
+                if (lector != null) lector.Close();
+                if (connection != null) connection.Close();
+            }*/
+            return retorno;
+
+        }
+    }
+}
