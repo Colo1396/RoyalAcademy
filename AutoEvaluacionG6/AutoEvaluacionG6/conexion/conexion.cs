7d5177b264b5e47a5b8695aff0ed23cb9e1687a6
Melisa Romero <melisa.e.romero@hotmail.com>
2019-10-25 16:05:03 -0300

    Baja Funcionando!!

--- AutoEvaluacionG6/AutoEvaluacionG6/conexion/conexion.cs
+++ AutoEvaluacionG6/AutoEvaluacionG6/conexion/conexion.cs
@@ -24,7 +24,7 @@ namespace AutoEvaluacionG6.conexion
     {
         public static MySqlConnection getConexion()
         {
-            string cn = "datasource=localhost;port=3306;username=root;password='root';database=autoevaluaciong6;"; 
+            string cn = "datasource=localhost;port=3306;username=root;password='';database=autoevaluaciong6;"; 
             //cambiado por Ro!
             return new MySqlConnection(cn);
         }
