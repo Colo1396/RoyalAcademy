82ea3a7adf72bf735d95ed34c6c3fd2cbb6fe0bd
Lucas Giannico <lucas.giannico.a@gmail.com>
2019-11-08 09:41:02 -0300

    corrector

---
+++ AutoEvaluacionG6/AutoEvaluacionG6/clases/GestorListas/GestorCarreras.cs
@@ -0,0 +1,23 @@
+﻿
+using AutoEvaluacionG6.clases.carreras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoEvaluacionG6.clases.GestorListas
+{
+    public class GestorCarreras
+    {
+        public virtual List<Carrera> lstCarreras { get; set; }
+
+
+
+        public GestorCarreras()
+        {
+            this.lstCarreras = new List<Carrera>();
+        }
+    }
+
+    
+}
